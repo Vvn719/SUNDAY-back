c51428cfe139d37bedd00c4b06f5c509a90edb20
Chang Ya-Han <vivianchang719@gmail.com>
2023-03-01 19:17:31 +0800

    Update

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,14 @@
 import { Router } from 'express'
 import content from '../middleware/content.js'
-import admin from '../middleware/admin.js'
+// import admin from '../middleware/admin.js'
 import upload from '../middleware/upload.js'
-import { jwt } from '../middleware/auth.js'
+// import { jwt } from '../middleware/auth.js'
 import { createProduct, getAllProducts, getProduct, getSellProducts, editProduct } from '../controllers/products.js'
 
 const router = Router()
 
-router.post('/', content('multipart/form-data'), jwt, admin, upload, createProduct)
+router.post('/', content('multipart/form-data'), upload, createProduct)
+// router.patch('/:id', content('multipart/form-data'), jwt, admin, upload, createProduct)
 // 查所有架上商品
 router.get('/', getSellProducts)
 // 取所有東西需要管理員，順序有差 先 all 再 id，all才不會被當成單個 id
@@ -15,6 +16,7 @@ router.get('/all', getAllProducts)
 // router.get('/all', jwt, admin, getAllProducts)
 // 取單個商品 直接 get id
 router.get('/:id', getProduct)
-router.patch('/:id', content('multipart/form-data'), jwt, admin, upload, editProduct)
+router.patch('/:id', content('multipart/form-data'), upload, editProduct)
+// router.patch('/:id', content('multipart/form-data'), jwt, admin, upload, editProduct)
 
 export default router
